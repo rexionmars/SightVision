22b0ee604a37c5d7d55543d5bed0981913347385
rexionmars <opensource.leonardi@gmail.com>
2023-07-22 18:50:09 -0300

    [bug] Fix all module imports

--- sightvision/__init__.py
+++ sightvision/__init__.py
@@ -4,3 +4,12 @@ from sightvision.ColorModule import ColorFinder
 from sightvision.FPS import FPS
 from sightvision.PIDModule import PID
 from sightvision.PlotModule import LivePlot
+from sightvision.FaceDetectionModule import FaceDetector
+from sightvision.HandTrackingModule import HandDetector
+from sightvision.PoseModule import PoseDetector
+
+from sightvision.ClassificationModule import Classifier
+
+from sightvision.SerialModule import SerialObject
+from sightvision.SelfiSegmentationModule import SelfiSegmentation
+from sightvision.FaceMeshModule import FaceMeshDetector
