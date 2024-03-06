39136849ef2e5218cbf26240dc54a71a90eeed3e
rexionmars <opensource.leonardi@gmail.com>
2024-03-06 06:52:37 -0300

    ref: Refactor FaceDetector class and update variable names

--- sightvision/FaceDetectionModule.py
+++ sightvision/FaceDetectionModule.py
@@ -1,3 +1,7 @@
+"""
+Face Detection Module
+Copyright (c) 2022 Leonardi Melo
+"""
 import cv2
 import mediapipe as mp
 
@@ -7,19 +11,21 @@ from Utils import stackImages
 
 class FaceDetector:
     """
-    Find faces in realtime using the light weight model provided in the
-    mediapipe library.
+    Class for detecting faces in an image using the MediaPipe Face Detection model.
     """
 
-    def __init__(self, minDetectionCon=0.5):
+    def __init__(self, min_detection_confidense=0.5):
         """
-        :param minDetectionCon: Minimum Detection Confidence Threshold
-        """
+        Initializes the FaceDetector object.
 
-        self.minDetectionCon = minDetectionCon
-        self.mpFaceDetection = mp.solutions.face_detection
-        self.mpDraw = mp.solutions.drawing_utils
-        self.faceDetection = self.mpFaceDetection.FaceDetection(self.minDetectionCon)
+        Args:
+            min_detection_confidense (float, optional): The minimum confidence threshold for face detection. Defaults to 0.5.
+        """
+        self.min_detection_confidense = min_detection_confidense
+        self.media_pipe_face_Fetection = mp.solutions.face_detection
+        self.media_pipe_draw = mp.solutions.drawing_utils
+        self.face_detection = self.media_pipe_face_Fetection.FaceDetection(
+            self.min_detection_confidense)
 
     def draw_detections(
         self,
@@ -35,13 +41,27 @@ class FaceDetector:
         thickness=1,
     ):
         """
-        Foo bar buzz
+        Draws the detections on the frame based on the specified parameters.
+
+        Args:
+            frame (numpy.ndarray): The input frame.
+            bbox (tuple): The bounding box coordinates.
+            x (int): The x-coordinate of the detection.
+            y (int): The y-coordinate of the detection.
+            cx (int): The x-coordinate of the center of the detection.
+            cy (int): The y-coordinate of the center of the detection.
+            detection (object): The detection object.
+            view_mode (int): The display mode number.
+            color (tuple, optional): The color of the drawn elements. Defaults to config.COLOR_LIGHT_YELLOW.
+            thickness (int, optional): The thickness of the drawn elements. Defaults to 1.
+        Raises:
+            Exception: If the display mode number is not 1 or 2.
+        Returns:
+            None
         """
         if view_mode not in [1, 2]:
-            raise Exception(
-                "The display mode number reported does not exist, please\
-                select 1 or 2"
-            )
+            raise Exception("The display mode number reported does not exist, please\
+                    select 1 or 2")
 
         match view_mode:
             case 1:
@@ -66,47 +86,48 @@ class FaceDetector:
             1,
         )
 
-    def findFaces(self, frame, view_mode=1, draw=True, color=(25, 220, 255)):
+    def find_faces(self, frame, view_mode=1, draw=True, color=(25, 220, 255)):
         """
-        Find faces in an image and return the bbox info
+        Finds faces in the given frame using the face detection model.
 
-        :param frame: Image to find the faces in.
-        :param view_mode: Mode of visualization (1 for rectangle, 2 for circle)
-        :param draw: Flag to draw the output on the image.
-        :return: Image with or without drawings. Bounding Box list.
+        Args:
+            frame (numpy.ndarray): The input frame in BGR format.
+            view_mode (int, optional): The view mode for drawing the detections. Defaults to 1.
+            draw (bool, optional): Whether to draw the detections on the frame. Defaults to True.
+            color (tuple, optional): The color for drawing the detections. Defaults to (25, 220, 255).
+
+        Returns:
+            tuple: A tuple containing the modified frame with detections and a list of bounding boxes.
         """
-
-        imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        self.results = self.faceDetection.process(imgRGB)
+        image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        self.results = self.face_detection.process(image_rgb)
         bboxs = []
 
         if self.results.detections:
             for id, detection in enumerate(self.results.detections):
-                bboxC = detection.location_data.relative_bounding_box
+                bbox_confidence = detection.location_data.relative_bounding_box
                 ih, iw, ic = frame.shape
 
                 bbox = (
-                    int(bboxC.xmin * iw),
-                    int(bboxC.ymin * ih),
-                    int(bboxC.width * iw),
-                    int(bboxC.height * ih),
+                    int(bbox_confidence.xmin * iw),
+                    int(bbox_confidence.ymin * ih),
+                    int(bbox_confidence.width * iw),
+                    int(bbox_confidence.height * ih),
                 )
 
                 cx, cy = bbox[0] + (bbox[2] // 2), bbox[1] + (bbox[3] // 2)
                 x = bbox[0]
                 y = bbox[1]
 
-                bboxInfo = {
+                bbox_info = {
                     "id": id,
                     "bbox": bbox,
                     "score": detection.score,
                     "center": (cx, cy),
                 }
-                bboxs.append(bboxInfo)
+                bboxs.append(bbox_info)
 
                 if draw:
-                    self.draw_detections(
-                        frame, bbox, x, y, cx, cy, detection, view_mode
-                    )
+                    self.draw_detections(frame, bbox, x, y, cx, cy, detection, view_mode, color)
 
         return frame, bboxs
