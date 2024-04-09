76e006185915afc0e94d2fecb80788e1d0ffdbaa
rexionmars <opensource.leonardi@gmail.com>
2024-04-09 11:48:08 -0300

    module: Refactor FaceDetector class and update draw_detections method

--- sightvision/module/face_detection.py
+++ sightvision/module/face_detection.py
@@ -5,8 +5,9 @@ Copyright (c) 2022 Leonardi Melo
 import cv2
 import mediapipe as mp
 
-import config
-from sightvision.utils.basics import stackImages
+from typing import Tuple, List, Dict, Any, Union, Optional
+
+from ..utils.basics import rounded_rectangle
 
 
 class FaceDetector:
@@ -15,30 +16,26 @@ class FaceDetector:
     """
 
     def __init__(self, min_detection_confidense=0.5):
-        """
-        Initializes the FaceDetector object.
-
-        Args:
-            min_detection_confidense (float, optional): The minimum confidence threshold for face detection. Defaults to 0.5.
-        """
         self.min_detection_confidense = min_detection_confidense
         self.media_pipe_face_Fetection = mp.solutions.face_detection
         self.media_pipe_draw = mp.solutions.drawing_utils
-        self.face_detection = self.media_pipe_face_Fetection.FaceDetection(
-            self.min_detection_confidense)
+        self.face_detection = self.media_pipe_face_Fetection.FaceDetection(self.min_detection_confidense)
 
     def draw_detections(
-        self,
-        frame,
-        bbox,
-        x,
-        y,
-        cx,
-        cy,
-        detection,
-        view_mode,
-        color=config.COLOR_LIGHT_YELLOW,
-        thickness=1,
+            self,
+            frame: object,
+            bbox: Tuple[int, int, int, int],
+            x: int,
+            y: int,
+            cx: int,
+            cy: int,
+            detection: object,
+            view_mode: int,
+            color=(25, 220, 255),
+            thickness=1,
+            external_info=False,
+            internal_info=False,
+            debug=False,
     ):
         """
         Draws the detections on the frame based on the specified parameters.
@@ -59,34 +56,83 @@ class FaceDetector:
         Returns:
             None
         """
+
+        padding_top = 17
+        padding_left = 5
+        bbox_confidence = detection.location_data.relative_bounding_box
+        relative_keypoints = detection.location_data.relative_keypoints
+
         if view_mode not in [1, 2]:
-            raise Exception("The display mode number reported does not exist, please\
-                    select 1 or 2")
+            raise Exception("The display mode number reported does not exist, please select 1 or 2")
 
         match view_mode:
             case 1:
-                # Draw a rectangle
-                cv2.line(frame, (x, y), (x - 30, y - 30), color, thickness)
-                cv2.rectangle(frame, bbox, color, thickness)
+                rounded_rectangle(frame, bbox, 20, 1, 0, color, color)
 
             case 2:
-                # Draw a circle
                 cv2.line(frame, (cx - 45, cy - 45), (x - 30, y - 30), color, thickness)
                 cv2.circle(frame, (cx, cy), 64, color, thickness)
 
-        # Label for text acuracy
-        cv2.rectangle(frame, (x - 145, y - 50, 115, 20), color, -1)
-        cv2.putText(
-            frame,
-            f"{int(detection.score[0] * 100)}% Acuracy",
-            (bbox[0] - 140, bbox[1] - 35),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.5,
-            (0, 0, 0),
-            1,
-        )
+        # Draw acuracy in internal box
+        if internal_info == True:
+            cv2.putText(frame, f"{int(detection.score[0] * 100)}% Acuracy", (bbox[0] + padding_left, bbox[1] + 17),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.3, (155, 155, 155), 1)
 
-    def find_faces(self, frame, view_mode=1, draw=True, color=(25, 220, 255)):
+        # Draw acuracy in external box
+        if external_info == True:
+            cv2.line(frame, (x, y), (x - 30, y - 30), color, thickness)
+            cv2.rectangle(frame, (x - 145, y - 50, 115, 20), color, -1)
+            cv2.putText(
+                frame,
+                f"{int(detection.score[0] * 100)}% Acuracy",
+                (bbox[0] - 140, bbox[1] - 35),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (0, 0, 0),
+                1,
+            )
+
+        if debug == True:
+            print(f"Detection:\n{detection}")
+            # Draw the bounding box coordinates
+            cv2.putText(frame, f"{int(detection.score[0] * 100)}% Acuracy",
+                        (bbox[0] + padding_left, bbox[1] + padding_top), cv2.FONT_HERSHEY_SIMPLEX, 0.3,
+                        (155, 155, 155), 1)
+            cv2.putText(frame, f"Xmin: {bbox_confidence.xmin:.4f}", (bbox[0] + padding_left, bbox[1] + 28),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.3, (155, 155, 155), 1)
+            cv2.putText(frame, f"Ymin: {bbox_confidence.ymin:.4f}", (bbox[0] + padding_left, bbox[1] + 38),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.3, (155, 155, 155), 1)
+            cv2.putText(frame, f"Width: {bbox_confidence.width:.4f}", (bbox[0] + padding_left, bbox[1] + 58),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.3, (155, 155, 155), 1)
+            cv2.putText(frame, f"Height: {bbox_confidence.height:.4f}", (bbox[0] + padding_left, bbox[1] + 68),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.3, (155, 155, 155), 1)
+
+            # Draw relative keypoints
+            cv2.putText(frame, f"Left Eye: {relative_keypoints[0].x:.4f}, {relative_keypoints[0].y:.4f}",
+                        (bbox[0] + padding_left, bbox[1] + 78), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (155, 155, 155), 1)
+            cv2.putText(frame, f"Right Eye: {relative_keypoints[1].x:.4f}, {relative_keypoints[1].y:.4f}",
+                        (bbox[0] + padding_left, bbox[1] + 88), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (155, 155, 155), 1)
+
+            # Draw precision label based on aproach
+            if detection.score[0] > 0.85:
+                cv2.putText(frame, "High Precision", (bbox[0] + padding_left, bbox[1] + 48),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.3, (155, 155, 155), 1)
+            elif detection.score[0] > 0.70:
+                cv2.putText(frame, "Medium Precision", (bbox[0] + padding_left, bbox[1] + 48),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.3, (155, 155, 155), 1)
+            else:
+                cv2.putText(frame, "Low Precision", (bbox[0] + padding_left, bbox[1] + 48),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.3, (155, 155, 155), 1)
+
+    def find_faces(self,
+                   frame,
+                   view_mode=1,
+                   draw=True,
+                   color=(25, 220, 255),
+                   thickness=1,
+                   external_info=False,
+                   internal_info=False,
+                   debug=False):
         """
         Finds faces in the given frame using the face detection model.
 
@@ -128,6 +174,7 @@ class FaceDetector:
                 bboxs.append(bbox_info)
 
                 if draw:
-                    self.draw_detections(frame, bbox, x, y, cx, cy, detection, view_mode, color)
+                    self.draw_detections(frame, bbox, x, y, cx, cy, detection, view_mode, color, thickness,
+                                         external_info, internal_info, debug)
 
         return frame, bboxs
